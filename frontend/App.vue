951022577ca0daa85df2b62d4fadd1852d1c9194
julian allen <julianallen111@gmail.com>
2026-05-20 19:58:44 +0700

    dev : integrasi vue.js

---
+++ frontend/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Finpoint</h1>
+    <p>Vue.js + Laravel Application</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
